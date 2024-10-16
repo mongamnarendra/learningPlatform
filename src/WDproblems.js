29753a73c638f2cc6f86b0b6cb00a705066224f8
narendramongam <mongamnarendra@gmail.com>
2024-10-16 17:08:06 +0530

    js is added

---
+++ src/WDproblems.js
@@ -0,0 +1,37 @@
+function increase(a) {
+    switch (a) {
+        case 1: var element = document.getElementById("time-bar");
+            element.style.height = "8%";
+            break;
+
+        case 2: var element = document.getElementById("time-bar");
+            element.style.height = "18%";
+            break;
+        case 3: var element = document.getElementById("time-bar");
+            element.style.height = "28%";
+            break;
+        case 4: var element = document.getElementById("time-bar");
+            element.style.height = "38%";
+            break;
+        case 5: var element = document.getElementById("time-bar");
+            element.style.height = "48%";
+            break;
+        case 6: var element = document.getElementById("time-bar");
+            element.style.height = "58%";
+            break;
+        case 7: var element = document.getElementById("time-bar");
+            element.style.height = "68%";
+            break;
+        case 8: var element = document.getElementById("time-bar");
+            element.style.height = "78%";
+            break;
+        case 9: var element = document.getElementById("time-bar");
+            element.style.height = "88%";
+            break;
+        case 10: var element = document.getElementById("time-bar");
+            element.style.height = "99%";
+            break;
+
+
+    }
+}
